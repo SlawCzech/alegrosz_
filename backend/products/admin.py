ff2333173b74b1dc09c5365688ba312fa011662c
SlawCzech <slaw.czech@gmail.com>
2023-03-03 19:10:40 +0100

    app for products with model and django cms

---
+++ backend/products/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from . import models
+
+class ProductAdminConfig(ModelAdmin):
+    model = models.Product
+
+    search_fields = ('name', 'price', 'description')
+    list_filter = ('name', 'price', 'description')
+    # ordering = ('-last_login',)
+    list_display = ('name', 'price', 'description')
+
+admin.site.register(models.Product, ProductAdminConfig)
