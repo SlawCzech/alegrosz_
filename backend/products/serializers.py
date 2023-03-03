5beaa957ac89713ca5652864e28ffd8144fc0e27
SlawCzech <slaw.czech@gmail.com>
2023-03-03 23:13:40 +0100

    create and delete products

---
+++ backend/products/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Product
+        fields = ('id', 'name', 'price', 'description')
+
---
+++ backend/products/viewsets.py
@@ -0,0 +1,36 @@
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from . import models
+from . import serializers
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+
+    def list(self, request, *args, **kwargs):
+        serializer = serializers.ProductSerializer(self.get_queryset(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, pk=None, **kwargs):
+        pass
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, pk=None, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, pk=None, **kwargs):
+        pass
+
+    def destroy(self, request, *args, pk=None, **kwargs):
+        product = get_object_or_404(queryset=self.get_queryset(), pk=pk)
+        product.delete()
+        return Response(data={}, status=status.HTTP_200_OK)
