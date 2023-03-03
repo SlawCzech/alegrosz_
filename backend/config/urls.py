8c450b70f3d2744d2514f7ee058236c40ac67247
SlawCzech <slaw.czech@gmail.com>
2023-03-03 23:13:40 +0100

    create and delete products

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,7 +19,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin123/', admin.site.urls),
-    path('api/', include('products.urls')),
+    path('api/v1/', include('products.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
